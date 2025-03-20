34968d8f03d3e03dac0316a06adcf76df2b9046f
em <158589637+MetaNurA@users.noreply.github.com>
2025-03-21 05:27:39 +0700

    Acara 9-10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,1 +1,25 @@
-gg
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('detail_profile')->insert([
+            'addres'=>'Nganjuk',
+            'nomor_telp'=>'08xxxxxxx',
+            'ttl'=>'2004-12-04',
+            'foto'=>'picture.png'
+       ]);
+    }
+}
+
