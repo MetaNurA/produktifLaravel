a5da79703fe51b74cf26cae65ec6a65abb101860
em <158589637+MetaNurA@users.noreply.github.com>
2025-03-21 04:52:41 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,13 +1,1 @@
-<?php
-namespace App\Http\Controllers\Frontend;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-    public function index()
-    {
-        return view('from');
-    }
-}
+sdfghj
